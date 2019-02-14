d01f2cfbe3ff8357cb4924678c620c6ed13307ee
Certil Remy <certil@noukod.com>
2019-02-14 18:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.open('GET', 'https://api.github.com/users/certilremy/repos');
   req.send();
   console.log(req);
 }
